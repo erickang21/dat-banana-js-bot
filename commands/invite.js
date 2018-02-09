a62d5407573223cd5207e8cacd9a59d0a4167ce3
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-02-09 16:13:22 -0500

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,16 @@
+exports.run = (client, message, args, level) => {
+   message.channel.send("Lemme join dat hood -> https://discordapp.com/oauth2/authorize?client_id=408746549070266368&scope=bot&permissions=8");
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "invite",
+  category: "Miscelaneous",
+  description: "Invite me to your server1",
+  usage: ".invite"
