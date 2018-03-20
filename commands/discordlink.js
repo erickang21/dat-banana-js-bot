b1395c7547778b1ea339ca66da77d0bf752a0cc1
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-03-20 18:00:44 -0400

    added discord command

--- commands/discordlink.js
+++ commands/discordlink.js
@@ -1,1 +1,17 @@
-
+exports.run = (client, message, args, level) => {
+    message.channel.send("Wanna join OUR hood? https://discord.gg/jmuXk5T is da wae!");
+ };
+ 
+ exports.conf = {
+   enabled: true,
+   guildOnly: false,
+   aliases: [],
+   permLevel: "User"
+ };
+ 
+ exports.help = {
+   name: "discord",
+   category: "Miscelaneous",
+   description: "Get the link to the hood (aka our Discord server)",
+   usage: ".discord"
+ };
