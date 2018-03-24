497cc61a7a628acd695957565638fe15df4594dd
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-03-24 19:47:41 -0400

    fancy embed for ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,12 +1,21 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-  const msg = await message.channel.send("Ping?");
-  msg.edit(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  const embed = new Discord.RichEmbed()
+    .setTitle("Pinging...")
+    .setDescription("Hang in there!")
+    .setColor(0x00ff00)
+  const msg = message.channel.send({ embed });
+  const em = new Discord.RichEmbed()
+    .setTitle("PoIIIING!")
+    .setDescription(`Bot Latency: ${msg.createdTimestamp - message.createdTimestamp}ms\nAPI Latency: ${Math.round(client.ping)}ms`)
+    .setColor(0x00ff00)
+    .setAuthor(`Sent by ${message.author.username}`, message.author.displayAvatarURL);
+  msg.edit(embed);
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [],
+  aliases: ["pong"],
   permLevel: "User"
 };
 
